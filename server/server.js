1ec10f350c37e39045261b66a7d25cdfd07d5bf1
A Punith Kumar <143724741+ArigalaPunithKumar@users.noreply.github.com>
2026-02-24 14:32:12 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -111,14 +111,8 @@ app.get('/api/bookings/:email', (req, res) => {
 });
 
 
-// ================= FRONTEND ROUTE =================
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-
 // Start Server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+
 });
